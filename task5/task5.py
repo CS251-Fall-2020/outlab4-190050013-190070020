d0ba9eb2b3fb22ea8d285734b56fb2b180def127
Dhruva Dhingra <dhruvadhingra@cse.iitb.ac.in>
2020-09-12 23:21:57 +0530

    Updated task5 to plot both scatter plot and linear least fit line and updated covid.png accordingly

--- task5/task5.py
+++ task5/task5.py
@@ -25,10 +25,11 @@ slope, intercept, r_value, p_value, std_err = linregress(days, h)
 num_days = int(np.ceil(((1 - intercept)/slope)))
 print(num_days)
 
+plt.scatter(days,h,c = 'r',marker = '.',label = fr'$H(t)$ vs t (in days)')
 days = np.linspace(1,num_days,num_days)
 h = [(intercept + slope * d) for d in days]
 h = np.array(h,dtype='float64')
-plt.plot(days,h, label = fr'$H(t)$ vs t (in days)')
+plt.plot(days,h, label = fr'Linear least squares fit')
 plt.xlabel("Time (in days)")
 plt.ylabel(fr"$H(t)$")
 plt.title(fr'$H(t)$ vs t')
